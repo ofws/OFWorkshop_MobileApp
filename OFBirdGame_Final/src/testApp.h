93c15c1659518b8a5b0e32ef07fa6861dada56cb
Peng Cheng <bestpaul1985@gmail.com>
2014-03-29 22:39:48 -0400

    update

---
+++ OFBirdGame_Final/src/testApp.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxiOS.h"
+#include "ofxiOSExtras.h"
+
+class testApp : public ofxiOSApp{
+	
+    public:
+        void setup();
+        void update();
+        void draw();
+        void exit();
+	
+        void touchDown(ofTouchEventArgs & touch);
+        void touchMoved(ofTouchEventArgs & touch);
+        void touchUp(ofTouchEventArgs & touch);
+        void touchDoubleTap(ofTouchEventArgs & touch);
+        void touchCancelled(ofTouchEventArgs & touch);
+
+        void lostFocus();
+        void gotFocus();
+        void gotMemoryWarning();
+        void deviceOrientationChanged(int newOrientation);
+    
+        float posx, posy;
+        ofPoint frc, vel, gravity;
+        float raidus;
+    
+    
+        //fire
+        float bombX[4];
+        float bombY[4];
+        float distance;
+        float bombRadius;
+        float speed;
+    
+        //star
+        float starx[2];
+        float stary[2];
+        float starRadius;
+    
+        bool bGameOver;
+    
+        //scole
+        int score;
+        ofTrueTypeFont font;
+    
+        ofImage start_Image;
+        ofImage bomb_Image;
+        ofImage brid_Image;
+        ofImage star_Image;
+};
+
+
